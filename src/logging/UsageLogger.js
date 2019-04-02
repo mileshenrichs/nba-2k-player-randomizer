cf7d7903b9cb2a0eb812b90a2d364c3a7de7204a
Miles <mileshenrichs21@gmail.com>
2019-04-01 22:08:09 -0500

    make AWS usage logging work

--- src/logging/UsageLogger.js
+++ src/logging/UsageLogger.js
@@ -1,15 +1,11 @@
 class UsageLogger {
-
     static apiUrl = 'https://4bwufafe7h.execute-api.us-east-1.amazonaws.com/default/log2kRandomizerUse';
 
     static sendLog(state) {
-        fetch(this.apiUrl + '?state=' + JSON.stringify(state), {
-            method: 'POST'
-        }).then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log('error!!!');
-            console.log(err);
+        fetch(this.apiUrl, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(state)
         });
     }
 }
